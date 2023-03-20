46c5fcc9ad2b1767b2ff1c66636e1e7583895dab
dev2502 <97377647+dev2502@users.noreply.github.com>
2023-03-20 15:06:13 +0530

    Update 30.js

--- 30.js
+++ 30.js
@@ -10,11 +10,11 @@ function guessNum() {
     attemptsCount++;
     const input = parseInt(document.forms[0].elements['input'].value);
 
-   
-    
+   if(input>=1 && input<= 1000 )
+   {
     if (input == randomNum) {
         document.getElementById("result").innerHTML = "GUESSED RIGHT!";
-        document.getElementById("attempts").innerHTML = `rigth guess attempts: ${attemptsCount}`;
+        document.getElementById("attempts").innerHTML = `rigth guess at ${attemptsCount} attempts !! `;
         clearText();
     } else if (input >= randomNum) {
         document.getElementById("result").innerHTML = "too high!!!";
@@ -25,6 +25,10 @@ function guessNum() {
         document.getElementById("attempts").innerHTML = `attempts: ${attemptsCount}`;
         clearText();
     }
+   }
+   else{
+      document.getElementById("result").innerHTML = "Enter number between 1 to 1000!!!";
+   }
 }
 
 function clearText() {
